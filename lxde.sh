f78d389033e5512cc0210208f78f3b67b029f32a
Daniel Tootill <dantootill@gmail.com>
2013-04-09 11:54:55 +0200

    Update lxde.sh

--- lxde.sh
+++ lxde.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+me=dan
+
 #pacman -S lxde gamin leafpad obconf epdfview gvfs polkit-gnome gnome-themes-standard libgnomeui
 pacman -S lxde gamin leafpad obconf epdfview gvfs polkit-gnome gnome-themes-standard
 
-su - dan -c 'mkdir -p $HOME/.config/openbox'
-su - dan -c 'cp /etc/xdg/openbox/{menu.xml,rc.xml,autostart} $HOME/.config/openbox'
-su - dan -c 'echo "exec startlxde" >> $HOME/.xinitrc'
+su - ${me} -c 'mkdir -p $HOME/.config/openbox'
+su - ${me} -c 'cp /etc/xdg/openbox/{menu.xml,rc.xml,autostart} $HOME/.config/openbox'
+su - ${me} -c 'echo "exec startlxde" >> $HOME/.xinitrc'
