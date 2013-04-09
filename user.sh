b8961a11c5cffe8a17e9293483f159372c2b3ab0
Daniel Tootill <dantootill@gmail.com>
2013-04-09 11:52:39 +0200

    Update user.sh

--- user.sh
+++ user.sh
@@ -6,6 +6,7 @@ groupadd networkmanager
 useradd -m -c "Dan Tootill" -g users -G audio,lp,networkmanager,optical,power,scanner,storage,video,wheel dan
 passwd dan
 
-su - dan -c "xdg-user-dirs-update"
+su - dan -c 'echo "export GTK2_RC_FILES=\"\$HOME/.gtkrc-2.0\"" >> $HOME/.xinitrc'
+su - dan -c 'xdg-user-dirs-update'
 
 sed -i "s/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers
