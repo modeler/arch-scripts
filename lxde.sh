85898a8405c4e0fc0d97d2af7e624adb8b1d3b77
Daniel Tootill <dantootill@gmail.com>
2013-04-09 11:40:54 +0200

    Update lxde.sh

--- lxde.sh
+++ lxde.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-pacman -S lxde gamin leafpad obconf epdfview gvfs polkit-gnome libgnomeui gnome-themes-standard
+#pacman -S lxde gamin leafpad obconf epdfview gvfs polkit-gnome gnome-themes-standard libgnomeui
+pacman -S lxde gamin leafpad obconf epdfview gvfs polkit-gnome gnome-themes-standard
 
-su - dan -c "mkdir -p /home/dan/.config/openbox"
-su - dan -c "cp /etc/xdg/openbox/{menu.xml,rc.xml,autostart} /home/dan/.config/openbox"
-
+su - dan -c 'mkdir -p $HOME/.config/openbox'
+su - dan -c 'cp /etc/xdg/openbox/{menu.xml,rc.xml,autostart} $HOME/.config/openbox'
+su - dan -c 'echo "exec startlxde" >> $HOME/.xinitrc'
+su - dan -c 'echo "export GTK2_RC_FILES=\"\$HOME/.gtkrc-2.0\"" >> $HOME/.xinitrc'
