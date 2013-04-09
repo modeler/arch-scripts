70091b994878585a79a4ab1c678e565b457b5f36
root <Dan Tootill>
2013-04-09 12:46:54 +0100

    Update user.sh

--- user.sh
+++ user.sh
@@ -8,6 +8,7 @@ groupadd networkmanager
 useradd -m -c "Dan Tootill" -g users -G audio,lp,networkmanager,optical,power,scanner,storage,video,wheel ${me}
 passwd dan
 
+su - ${me} -c 'echo "gtk-theme-name=\"Adwaita\"" > $HOME/.gtkrc-2.0'
 su - ${me} -c 'echo "export GTK2_RC_FILES=\"\$HOME/.gtkrc-2.0\"" >> $HOME/.xinitrc'
 su - ${me} -c 'xdg-user-dirs-update'
 
