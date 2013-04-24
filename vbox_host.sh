90213a646b103b90d1365bb82448e9c4bcdc0d45
Dan Tootill <dantootill@gmail.com>
2013-04-24 07:47:58 +0100

    Update vbox_host.sh

--- vbox_host.sh
+++ vbox_host.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-pacman -S virtualbox
+pacman -S virtualbox virtualbox-guest-iso net-tools
 
 cat > /etc/modules-load.d/virtualbox.conf << EOF
 vboxdrv
+vboxnetadp
+vboxnetflt
+vboxpci
 EOF
+
+gpasswd -a dan vboxusers
 
