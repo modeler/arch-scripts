0acbd600f7ad72bb4f97c5393fa91c93a2d9c913
Dan Tootill <dantootill@gmail.com>
2013-04-24 14:27:32 +0100

    Fixed typo!

--- chroot.sh
+++ chroot.sh
@@ -10,7 +10,7 @@ echo "KEYMAP=uk" > /etc/vconsole.conf
 
 sed -i "s/^#en_GB.UTF-8/en_GB.UTF-8/" /etc/locale.gen
 locale-gen
-echo "LANG=en_GB.UTF.8" > /etc/locale.conf
+echo "LANG=en_GB.UTF-8" > /etc/locale.conf
 
 syslinux-install_update -iam
 vi /boot/syslinux/syslinux.cfg
