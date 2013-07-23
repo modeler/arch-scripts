a7712434e4bccbcc2070331b5af93e20dfbb8be6
Daniel Tootill <dantootill@gmail.com>
2013-07-23 13:38:39 +0100

    Update networkmanager.sh

--- networkmanager.sh
+++ networkmanager.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 pacman -S networkmanager network-manager-applet gnome-keyring
-systemctl enable NetworkManager
 
 ln -s '/usr/lib/systemd/system/NetworkManager.service' '/etc/systemd/system/multi-user.target.wants/NetworkManager.service'
-
