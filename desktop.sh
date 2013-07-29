9c1d42120ba4f7fe62c87bc82b82c0a12693e299
Daniel Tootill <dantootill@gmail.com>
2013-07-29 15:19:46 +0100

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -5,45 +5,36 @@ pacman -S ttf-ms-fonts ttf-vista-fonts gsfonts xcursor-vanilla-dmz
 cat > /etc/fonts/local.conf << EOF
 <?xml version="1.0"?>
 <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-
 <fontconfig>
-
 	<match target="font">
 		<edit name="antialias" mode="assign">
 			<bool>true</bool>
 		</edit>
 	</match>
-
 	<match target="font">
 		<edit name="hinting" mode="assign">
 			<bool>true</bool>
 		</edit>
 	</match>
-
 	<match target="font">
 		<edit name="autohint" mode="assign">
 			<bool>false</bool>
 		</edit>
 	</match>
-
 	<match target="font">
 		<edit name="hintstyle" mode="assign">
 			<const>hintslight</const>
 		</edit>
 	</match>
-
 	<match target="font">
 		<edit name="rgba" mode="assign">
 			<const>rgb</const>
 		</edit>
 	</match>
-
 	<match target="font">
 		<edit mode="assign" name="lcdfilter">
 			<const>lcddefault</const>
 		</edit>
 	</match>
-
 </fontconfig>
 EOF
-
