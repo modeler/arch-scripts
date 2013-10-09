28eaa032ce1608e2f26716a063803b9acafcb4bd
Dan Tootill <dantootill@gmail.com>
2013-10-09 23:40:03 +0100

    Removed polkit-gnome.

--- lxde.sh
+++ lxde.sh
@@ -2,7 +2,7 @@
 
 me=dan
 
-pacman -S lxde gamin leafpad obconf epdfview gvfs polkit-gnome gnome-themes-standard faenza-icon-theme
+pacman -S lxde gamin leafpad obconf epdfview gvfs gnome-themes-standard faenza-icon-theme
 
 su - ${me} -c 'mkdir -p $HOME/.config/openbox'
 su - ${me} -c 'cp /etc/xdg/openbox/{menu.xml,rc.xml,autostart} $HOME/.config/openbox'
