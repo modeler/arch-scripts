87b3fdf01d8ac73a37463c7476206a9d50308aa9
Dan Tootill <dantootill@gmail.com>
2013-10-14 10:23:05 +0100

    SSD script updates.

--- ssd.sh
+++ ssd.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-sed -i "s/ro$/ro elevator=noop/" /boot/syslinux/syslinux.cfg
+sed -i "s/rw/rw elevator=noop/" /boot/syslinux/syslinux.cfg
 
-sed -i "s/data=ordered/data=ordered,discard/" /etc/fstab
+sed -i "s/relatime/noatime,discard/" /etc/fstab
 
-sed -i "s/#vm.vfs_cache_pressure = 100/vm.vfs_cache_pressure = 50/" /etc/sysctl.conf
-
-sed -i "s/#vm.swappiness = 60/vm.swappiness = 1/" /etc/sysctl.conf
+echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.d/99-sysctl.conf
+echo "vm.swappiness = 1" >> /etc/sysctl.d/99-sysctl.conf
 
