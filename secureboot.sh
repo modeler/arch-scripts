e312d155dc9d792a178b2af9e92c99ff7f754a09
root <root@localhost.localdomain>
2014-08-22 09:05:45 +0000

    Experimental script for UEFI support with Secure Boot enabled.

---
+++ secureboot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+os=$(ls -l /dev/disk/by-uuid/ | awk '/sda2/ { print $9 }')
+
+pacman -S gummiboot
+
+mkdir -p /boot/loader/entries
+cat >> /boot/loader/loader.conf << EOF
+default	arch
+timeout	10
+EOF
+
+cat >> /boot/loader/entries/arch.conf << EOF
+title	Arch Linux
+linux	/vmlinuz-linux
+initrd	/initramfs-linux.img
+options	root=UUID=${os} rw
+EOF
+
+cp /boot/EFI/gummiboot/gummibootx64.efi /boot/EFI/loader.efi
+
+cd /boot/EFI
+wget http://blog.hansenpartnership.com/wp-uploads/2013/PreLoader.efi
+wget http://blog.hansenpartnership.com/wp-uploads/2013/HashTool.efi
+
+exit 0
