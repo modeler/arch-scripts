72119745ac82b35b72ae21c677e0b04d4e9dccbd
Daniel Tootill <daniel.tootill@thomsonreuters.com>
2017-09-06 13:57:50 +0100

    Re-familiarising myself with git.

--- mate.sh
+++ mate.sh
@@ -14,3 +14,4 @@ pacman -Sy mate mate-extras gtk-engine-murrine
 su - ${me} -c 'echo "exec mate-session" >> $HOME/.xinitrc'
 
 sed -i -e "s/e8e8e8/fdfdfd/g" -e "s/e2e2e2/f5f5f5/g" -e "s/dcdcdc/ededed/g" /usr/share/themes/Menta/metacity-1/metacity-theme-1.xml
+
